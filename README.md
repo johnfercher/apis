ba43c8b4a54c5bb19aac8ad911b2f0212c7ff974
jfercher <johnathanfercher22@gmail.com>
2021-09-12 20:57:13 -0300

    Rust api example done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# apis
+# APIs
+
+## RustAPI
+Based on Article: [Creating a Rest API in Rust With Warp][rustapi]
+
+[rustapi]: https://blog.logrocket.com/creating-a-rest-api-in-rust-with-warp/
--- rustapi/src/main.rs
+++ rustapi/src/main.rs
@@ -1,3 +1,110 @@
-fn main() {
-    println!("Hello, world!");
+mod entity;
+
+use warp::{http, Filter};
+use entity::{Store,Item,Id};
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let store = Store::new();
+    let store_filter = warp::any().map(move || store.clone());
+
+    let add_items = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("groceries"))
+        .and(warp::path::end())
+        .and(json_body())
+        .and(store_filter.clone())
+        .and_then(add_grocery_list_item);
+
+    let get_items = warp::get()
+        .and(warp::path("v1"))
+        .and(warp::path("groceries"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(get_grocery_list);
+
+    let update_item = warp::put()
+        .and(warp::path("v1"))
+        .and(warp::path("groceries"))
+        .and(warp::path::end())
+        .and(json_body())
+        .and(store_filter.clone())
+        .and_then(update_grocery_list);
+
+    let delete_item = warp::delete()
+        .and(warp::path("v1"))
+        .and(warp::path("groceries"))
+        .and(warp::path::end())
+        .and(id_json())
+        .and(store_filter.clone())
+        .and_then(delete_grocery_list_item);
+
+    let routes = add_items.or(get_items).or(delete_item).or(update_item);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+fn json_body() -> impl Filter<Extract = (Item,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+fn id_json() -> impl Filter<Extract = (Id,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+async fn add_grocery_list_item(
+    item: Item,
+    store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    store.grocery_list.write().insert(item.name, item.quantity);
+
+    Ok(warp::reply::with_status(
+        "Added items to the grocery list",
+        http::StatusCode::CREATED,
+    ))
+}
+
+async fn get_grocery_list(
+    store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut result = HashMap::new();
+    let r = store.grocery_list.read();
+
+
+    for (key,value) in r.iter() {
+        result.insert(key, value);
+    }
+
+    Ok(warp::reply::json(
+        &result
+    ))
+}
+
+async fn delete_grocery_list_item(
+    id: Id,
+    store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    store.grocery_list.write().remove(&id.name);
+
+
+    Ok(warp::reply::with_status(
+        "Removed item from grocery list",
+        http::StatusCode::OK,
+    ))
+}
+
+async fn update_grocery_list(
+    item: Item,
+    store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    store.grocery_list.write().insert(item.name, item.quantity);
+
+
+    Ok(warp::reply::with_status(
+        "Added items to the grocery list",
+        http::StatusCode::CREATED,
+    ))
 }
