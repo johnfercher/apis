8d2779aade55edfa546f5626509776d58449a7c6
jfercher <johnathanfercher22@gmail.com>
2021-09-14 22:26:35 -0300

    Automation

--- generate_files_quantity.sh
+++ generate_files_quantity.sh
@@ -3,7 +3,8 @@
 version=$1
 
 cs="$(cloc netcoreapi --exclude-dir=bin,obj,Migrations --include-ext=cs --csv | tail -n 1 | cut -d',' -f1)"
+csm="$(cloc Netcoreapi-Minimal --exclude-dir=bin,obj,Migrations --include-ext=cs --csv | tail -n 1 | cut -d',' -f1)"
 rs="$(cloc rustapi --exclude-dir=target --include-ext=rs --csv | tail -n 1 | cut -d',' -f1)"
 py="$(cloc pythondjango --exclude-dir migrations --include-ext=py --csv | tail -n 1 | cut -d',' -f1)"
 
-printf "C# $cs\nRust $rs\nPython $py"> "docs/$version/data/files_quantity.dat"
+printf "C# $cs\nC#(Minimal) $csm\nRust $rs\nPython $py" > "docs/$version/data/files_quantity.dat"
--- generate_lines_quantity.sh
+++ generate_lines_quantity.sh
@@ -3,7 +3,8 @@
 version=$1
 
 cs="$(cloc netcoreapi --exclude-dir=bin,obj,Migrations --include-ext=cs --csv | tail -n 1 | cut -d',' -f5)"
+csm="$(cloc Netcoreapi-Minimal --exclude-dir=bin,obj,Migrations --include-ext=cs --csv | tail -n 1 | cut -d',' -f5)"
 rs="$(cloc rustapi --exclude-dir=target --include-ext=rs --csv | tail -n 1 | cut -d',' -f5)"
 py="$(cloc pythondjango --exclude-dir migrations --include-ext=py --csv | tail -n 1 | cut -d',' -f5)"
 
-printf "C# $cs\nRust $rs\nPython $py"> "docs/$version/data/lines_quantity.dat"
+printf "C# $cs\nC#(Minimal) $csm\nRust $rs\nPython $py" > "docs/$version/data/lines_quantity.dat"
