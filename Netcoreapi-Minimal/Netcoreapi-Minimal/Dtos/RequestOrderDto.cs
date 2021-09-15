1f59bd5a50e16c877bb174489e8807677ded3cc0
Felipe Amaral <felipemelloamaral@gmail.com>
2021-09-14 21:33:44 -0300

    - Create netcore minimal api

---
+++ Netcoreapi-Minimal/Netcoreapi-Minimal/Dtos/RequestOrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Netcoreapi_Minimal.Dtos
+{
+    public class RequestOrderDto
+    {
+        public string LabelCode { get; set; }
+        public string Origin { get; set; }
+        public string Destiny { get; set; }
+    }
+}
